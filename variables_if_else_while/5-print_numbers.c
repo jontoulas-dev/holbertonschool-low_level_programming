c17e782b0a907360bc720ae18547e9e2ad171726
Jonathan Toulas <jontoulas@gmail.com>
2026-06-23 11:22:02 +0200

    number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int number = 1;
+
+while (number <= 9)
+{
+printf("%d", number);
+number++;
+}
+putchar('\n');
+return (0);
+}
