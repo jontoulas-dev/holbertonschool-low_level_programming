86d8f3874a5f33db89f388bdebc73ae766d28e78
Jonathan Toulas <jontoulas@gmail.com>
2026-06-24 11:27:59 +0200

    while while alpha

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times, followed by a new line.
+*
+* Return: void.
+*/
+void print_alphabet_x10(void)
+{
+char lettre;
+int tour;
+
+tour = 0;
+while (tour < 10)
+{
+lettre = 'a';
+
+while (lettre <= 'z')
+{
+_putchar(lettre);
+lettre++;
+}
+_putchar('\n');
+tour++;
+}
+
+_putchar('\n');
+}
