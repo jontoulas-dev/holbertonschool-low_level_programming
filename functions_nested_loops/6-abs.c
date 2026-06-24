688c29e8a116a663d0e652e3fee3817f2fc2f570
Jonathan Toulas <jontoulas@gmail.com>
2026-06-24 14:50:31 +0200

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Absolute value of an integer
+ * @n: the integer to check
+ * Return: Always absolute value
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
