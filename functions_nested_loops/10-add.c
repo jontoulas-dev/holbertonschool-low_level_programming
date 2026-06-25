0187db02f51f1f58a552e1c435107c4f2c766b54
Jonathan Toulas <jontoulas@gmail.com>
2026-06-25 10:41:55 +0200

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Print table of 9
+ *@a: premier entier
+ *@b: deuxieme entier
+ * Return: resulat de a plus b
+ */
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+
+return (result);
+}
