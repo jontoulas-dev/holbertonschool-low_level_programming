2a3a889f7e706bc2c4cebc3b6276c5968db6d340
Jonathan Toulas <jontoulas@gmail.com>
2026-06-25 10:58:31 +0200

    print 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print table of 9
+ *@n: nombre qui cherche ç_
+ *
+ *
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%d ,", n);
+}
+for (; n > 98; n--)
+{
+printf("%d ,", n);
+}
+}
+printf("\n");
+}
