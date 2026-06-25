ba9f91e670bc568b06efec4ab5afa09ccd2f2046
Jonathan Toulas <jontoulas@gmail.com>
2026-06-25 09:58:47 +0200

    beaucoup de if

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Print table of 9
+ *
+ *
+ */
+void times_table(void)
+{
+int l;
+int c;
+int produit;
+
+for (l = 0; l < 10; l++)
+{
+for (c = 0; c < 10; c++)
+{
+produit = l * c;
+
+if (c > 0)
+{
+_putchar(',');
+
+if (produit < 10)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(' ');
+}
+}
+if (produit > 10)
+{
+_putchar(produit + '0');
+}
+else
+{
+_putchar((produit / 10) + '0');
+_putchar((produit % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
