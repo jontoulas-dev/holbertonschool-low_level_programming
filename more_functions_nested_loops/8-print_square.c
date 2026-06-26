1d0b24ba7ee62bc3981a53b6307f46a722d63524
Jonathan Toulas <jontoulas@gmail.com>
2026-06-26 15:17:57 +0200

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Print square
+ * @size: le nombre de fois à afficher
+ *
+ */
+void print_square(int size)
+{
+int ligne, colonne;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (ligne = 0; ligne < size; ligne++)
+{
+for (colonne = 0; colonne < size; colonne++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
