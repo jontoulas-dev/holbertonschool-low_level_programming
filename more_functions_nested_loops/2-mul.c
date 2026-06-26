54d1595351a772913cef79296a209c4e9af3fc3c
Jonathan Toulas <jontoulas@gmail.com>
2026-06-26 10:37:29 +0200

    a*b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplie deux entiers
+ * @a: Premier entier
+ * @b: Second entier
+ * Return: result
+ */
+int mul(int a, int b)
+{
+return  (a * b);
+}
