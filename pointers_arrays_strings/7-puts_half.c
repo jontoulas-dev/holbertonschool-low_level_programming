3dbd15420e1567628d5b689b95a31db7c0a8cb8e
Jonathan Toulas <jontoulas@gmail.com>
2026-06-30 22:01:11 +0200

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - print a string
+ * @str: point the caracter
+ *
+ */
+void puts_half(char *str)
+{
+int longueur = 0;
+int i;
+
+while (str[longueur] != '\0')
+{
+longueur++;
+}
+
+if (i % 2 == 0)
+{
+i = longueur / 2;
+}
+else
+{
+i = (longueur + 1) / 2;
+}
+while (str[i] != '\0')
+{
+_putchar(str[i] != '\0');
+i++;
+}
+_putchar('\n');
+}
