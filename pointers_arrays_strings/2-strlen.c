98c30eb4a8e0f9927fd15dcbce06c0a59ea26534
Jonathan Toulas <jontoulas@gmail.com>
2026-06-30 11:22:03 +0200

    for s[i]

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - computes the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+return (i);
+}
