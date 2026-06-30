b3dab1735dbb962f2c31c6aec57b2ff4e85c6f31
Jonathan Toulas <jontoulas@gmail.com>
2026-06-30 16:54:15 +0200

    calcu

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-int choice = -1;
+int choice = 1;
 int A = 0;
 int B = 0;
 int result = 0;
@@ -12,7 +12,7 @@ printf("Simple Calculator\n");
 while (choice != 0)
 {
 printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
-printf("choice ?\n");
+printf("Choice ?\n");
 scanf("%d", &choice);
 if (choice == 0)
 {
