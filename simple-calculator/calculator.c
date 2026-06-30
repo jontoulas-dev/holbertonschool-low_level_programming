3a7ca388945d3b2ddab0b86222d6e01af4003e24
Jonathan Toulas <jontoulas@gmail.com>
2026-06-30 17:15:59 +0200

    calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for the simple calculator program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int choice = 1;
-int A = 0;
-int B = 0;
-int result = 0;
+	int choice = 1;
+	int A = 0;
+	int B = 0;
+	int result = 0;
 
-printf("Simple Calculator\n");
-
-while (choice != 0)
-{
-printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
-printf("Choice: ");
-scanf("%d", &choice);
-if (choice == 0)
-{
-printf("Bye!\n");
-}
-}
-return(0);
+	printf("Simple Calculator\n");
+	while (choice != 0)
+	{
+		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
+		printf("Choice: ");
+		scanf("%d", &choice);
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
+	}
+	return (0);
 }
