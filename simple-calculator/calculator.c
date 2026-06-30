d7bfd66858058513bbb4f116f6bedc19bad69c89
Jonathan Toulas <jontoulas@gmail.com>
2026-06-30 17:12:13 +0200

    calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -12,7 +12,7 @@ printf("Simple Calculator\n");
 while (choice != 0)
 {
 printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
-printf("Choice:");
+printf("Choice: ");
 scanf("%d", &choice);
 if (choice == 0)
 {
